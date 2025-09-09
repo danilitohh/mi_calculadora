2f28f0f68755ae98a4c057dc7969dd099e141f5b
danilo herrera hernandez <111337799+danilitohh@users.noreply.github.com>
2025-09-09 11:54:22 -0500

    Agregar función potencia y comentario

--- calculadora.py
+++ calculadora.py
@@ -16,8 +16,15 @@ def dividir(a: float, b: float) -> float:
         raise ValueError("No se puede dividir por cero.")
     return a / b
 
+def potencia(a: float, b: float) -> float:
+    #Calcula a elevado a la b. (modificación para forzar commit)
+    return a**b
+
+
 if __name__ == "__main__":
     print(sumar(2, 3))          #5
     print(restar(5, 1))         #4
     print(multiplicar(4, 2))    #8
     print(dividir(10, 2))       #5.0
+    print(potencia(2, 3))       #8
+
